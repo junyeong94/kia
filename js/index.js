674fed5b01670441902971ea35682e3fb8a15643
junyeong94 <64447875+junyeong94@users.noreply.github.com>
2020-05-25 17:00:59 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -30,20 +30,17 @@ $(function(){
   for(var cnt=0;cnt<rotateItem.length;cnt++){
     rotateItem.eq(cnt).css('animation-delay',(cnt+10)*30+'ms');
   }
-  
   // 버거이벤트
   $('#tgl_nav_btn').click(function(){
     $(this).toggleClass('active');
     $('.nav').toggleClass('active');
+    if($(this).attr('class')!='active'){
+      $('.sub_nav,.car_list,.large_nav>.content>ul>li').removeClass('active');
+    }
   })
 
-  // en 호버이벤트
   $('.search').before('<li class="en"><a href="#">EN</a></li>');
-  
-  // 윈도우 리사이즈 이벤트
   $(window).resize(function(){
-    $('.nav_btn').parent().removeClass('active');
-    $('.nav_btn').next().removeClass('active');
     var winWidth=$(window).width();
     if(winWidth>=770){
       $('.en').hide();
@@ -51,6 +48,96 @@ $(function(){
       $('.en').show();
     }
   });
-    
 
+  // 메인 슬라이더
+  var mySwiper = new Swiper ('.main_slider', {
+    speed:500,
+    effect:'fade',
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type:'bullets',
+    },
+    autoplay: {
+      delay: 5000,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+
+  // 스타트/스톱 이벤트
+  $('#start_btn').click(function(){
+    $('.start_stop_btn>button').hide();
+    $('#stop_btn').show();
+    mySwiper.autoplay.start();
+  });
+  $('#stop_btn').click(function(){
+    $('.start_stop_btn>button').hide();
+    $('#start_btn').show();    
+    mySwiper.autoplay.stop();
+  });
+
+  // 베스트기아 슬라이더
+  var mySwiper2 = new Swiper ('.best_kia_slider', {
+    speed:300,
+    spaceBetween:0,
+    slidesPerView:1.5,
+    centeredSlides:true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type:'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints:{
+      770: {
+        slidesPerView:3.5,
+      },
+    },
+  });
+//   mySwiper2.on('slideChange', function () {
+//  });
+
+// 이벤트 기아 슬라이더
+var mySwiper3 = new Swiper ('.event_kia_slider', {
+  speed:300,
+  slidesPerView:1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type:'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+// 채널 기아 슬라이더
+var mySwiper4 = new Swiper ('.channel_kia_slider', {
+  speed:300,
+  slidesPerView:1,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type:'bullets',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 })
